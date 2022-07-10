d93c53339d576a3674d6fee891ff4ee90d54d925
rafael <rafaworksi@outlook.com>
2022-07-10 00:17:12 -0300

    feat: criacao AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    
+    public function login(Request $request){
+
+        $credenciais = $request->all(['email', 'password']);
+        $token= auth('api')->attempt($credenciais);
+
+        if($token){
+            return response()->json(['token'=> $token]);
+        }else{
+            return response()->json(['msg'=> 'usuário ou senha inválido!'], 403);
+        }
+    }
+
+    public function logout(){
+        auth('api')->logout();
+        return response()->json(['msg'=>'Logout realizado com sucesso!']);
+    }
+
+    public function refresh(){
+        $token = auth('api')->refresh();
+        return response()->json(['token'=> $token]);
+    }
+
+    public function me(){
+        return response()->json(auth()->user());
+    }
+
+
+}
