e8338d78e6d07613ed4b47751f31a3b4db21efbd
rafael <rafaworksi@outlook.com>
2022-07-10 00:26:52 -0300

    refactor: inclusao de mais um campo na migration user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Hash;
 
 class CreateUsersTable extends Migration
 {
@@ -12,16 +14,26 @@ class CreateUsersTable extends Migration
      * @return void
      */
     public function up()
-    {
+    {   
+        Schema::dropIfExists('users');                                                  
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('cargo', ['admin', 'cozinheiro', 'garcom']);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::insert('insert into users (name, cargo, email, password) values (?,?,?,?)',['Admin', 'admin', 'admin@', Hash::make('1234')]);
+
+        DB::insert('insert into users (name, cargo, email, password) values (?,?,?,?)',['Pedro', 'cozinheiro', 'pedro@', Hash::make('1234')]);
+
+        DB::insert('insert into users (name, cargo, email, password) values (?,?,?,?)',['Luiz', 'garcom', 'luiz@', Hash::make('1234')]);
+
+        DB::insert('insert into users (name, cargo, email, password) values (?,?,?,?)',['Deleon', 'garcom', 'deleon@', Hash::make('1234')]);
     }
 
     /**
