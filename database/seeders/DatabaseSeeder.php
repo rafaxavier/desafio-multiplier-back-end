c632b76a77c4ec04418ed86da40aed3ec75243f0
rafael <rafaworksi@outlook.com>
2022-07-10 00:36:30 -0300

    feat: declaracao de factorys no DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Cliente::factory()->count(10000)->create();
+        \App\Models\Cardapio::factory()->count(50)->create();
+        \App\Models\Pedido::factory()->count(400)->create();
+        \App\Models\ItemPedido::factory()->count(800)->create();
     }
 }
