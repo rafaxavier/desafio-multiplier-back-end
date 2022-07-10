4ccf47723216f70891f9e56a1e551c6e0de49b81
rafael <rafaworksi@outlook.com>
2022-07-10 00:34:32 -0300

    feat: criacao de factory para os models Cardapio e Cliente

---
+++ database/factories/ClienteFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ClienteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nome' => $this->faker->name(),
+            'cpf' => $this->faker->unique()->bothify('###.###.###-##') ,
+        ];
+    }
+}
